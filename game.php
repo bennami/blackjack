9c75187466edbc4c511abd4f165b5c14a23d573d
imane <benbenimane@hotmail.com>
2020-02-13 11:03:19 +0100

    methods added, taking break

--- game.php
+++ game.php
@@ -2,10 +2,27 @@
 class BlackJack{
 
 //property score
-
  public $score = 'score';
 
-//hit stand and surrender methods
+
+
+//hit adds a card between 1 and 11
+function hit(){
+    $randomNumber=  rand(1,11);
+}
+
+// should end your turn and start the dealer's turn. (Your point total is saved.)
+function stand(){
+
+}
+
+//Surrender should make you surrender the game. (Dealer wins.)
+function surrender(){
+
+}
+
+
+
 
 
 }
