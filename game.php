305f2daa0717803de0284d08b234e938e258b90d
imane <benbenimane@hotmail.com>
2020-02-13 10:41:47 +0100

    creating  files, creating class and checking  out how to use OOP

---
+++ game.php
@@ -0,0 +1,13 @@
+<?php
+class BlackJack{
+
+//property score
+
+ public $score = 'score';
+
+//hit stand and surrender methods
+
+
+}
+
+$object =  new BlackJack();
---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+echo 'this is a blackjack game with OOP';
+
+
+
+
+
+
+
+
+
+?>
+<<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Black Jack</title>
+</head>
+<body>
+
+</body>
+</html>
+
