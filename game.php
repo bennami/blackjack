6b604340fd775002099d6c9ce660199946ec5fa4
imane <benbenimane@hotmail.com>
2020-02-13 17:16:14 +0100

    on start generate two cards and put score in session

--- game.php
+++ game.php
@@ -3,24 +3,24 @@
 class BlackJack{
     //property score. This property should have the value of the player's score at all times.
     public $score = 0;
-    public $randomNumber1 = 0;
-    public  $randomNumber = 0;
-    //hit adds a card between 1 and 11
+
+    //hit adds 2 cards between 1 and 11 and puts the score in the session
     function hit(){
-        if(isset($_POST['hit'])){
+
             $randomNumber = array();
             $randomNumber1 = rand(1, 11);
             $randomNumber2 = rand(1, 11);
             array_push($randomNumber, $randomNumber1, $randomNumber2);
             array_sum($randomNumber);
-            echo $this->score = implode (' ',$randomNumber);
-            echo $_SESSION['score']= array_sum($randomNumber);
-        }else{
-            $this->score;
-            $this->randomNumber1=0;
-            $this->randomNumber2=0;
+            echo $this->score = implode ('<br>',$randomNumber);
+            echo $_SESSION['score']= 'your score is'.array_sum($randomNumber);
         }
-        }
+
+    //draws a card and adds that to session score
+    function hitPlayer(){
+        $randomNumber = rand(1, 11);
+        echo $_SESSION['score'] += $randomNumber;
+    }
         /*if (array_sum(explode('  ', $randomNumber)) <= 21){
            echo $this->score = 'BLACK JACK';
         }*/
@@ -38,18 +38,14 @@ class BlackJack{
 //instantiate two objects
 $player = new BlackJack();
 $dealer = new BlackJack();
-$player->hit();
-    if (isset($_GET['start'])) {
-    $player->score;
+
+if (isset($_GET['start'])) {
+    //$player->hit();
         //refresh page to start game
         if (isset($_POST['hit'])) {
-            function hitPlayer(){
-                $player->hit();
-                // header('refresh: 0');
-
-            }
+         echo $player->hitPlayer();
         } else{
-            $player->score;
+            echo$player->hit();
         }
 
     }
